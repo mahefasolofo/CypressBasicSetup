6424297c6ae5b161a14bbd2eaf66bec7b040e5cf
Rija William RALITERA <rijawilliamralitera@macbook-pro-de-rija.home>
2023-01-18 22:21:35 +0100

    add new config

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   reporter: 'mochawesome',
   reporterOptions: {
     reportDir: 'cypress/results',
-    overwrite: false,
+    overwrite: true,
     html: false,
     json: true,
   },
